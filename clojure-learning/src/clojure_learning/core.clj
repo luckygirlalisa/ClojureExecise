485880c19bb7f76034b02bd4ee3dc2905c5ea454
luckygirlalisa <zfxiao@thoughtworks.com>
2014-06-10 11:44:48 +0800

    add test for every test

--- clojure-learning/src/clojure_learning/core.clj
+++ clojure-learning/src/clojure_learning/core.clj
@@ -6,9 +6,6 @@
   [& args]
   (println "Hello, World!"))
 
-;; Anything you type in here will be executed
-;; immediately with the results shown on the
-;; right.
 (defn abs
   [x]
   (cond
@@ -16,27 +13,21 @@
    :else x
   )
 )
-(abs -3)
-(abs 3)
 
 (defn square
   [x]
   (* x x)
 )
-(square 2)
 
 (defn good_enough
   [x guess]
   (< (abs (- x (square guess))) 0.001)
 )
-(good_enough 4 2.001)
-(good_enough 4 2.0001)
 
 (defn improve
   [x guess]
   (/ (+ guess (/ x guess)) 2)
 )
-(improve 4 1)
 
 (defn sqrt
   [x guess]
@@ -45,4 +36,3 @@
    :else (sqrt x (improve x guess))
    )
 )
-(sqrt 4 1)
--- clojure-learning/test/clojure_learning/core_test.clj
+++ clojure-learning/test/clojure_learning/core_test.clj
@@ -2,12 +2,27 @@
   (:require [clojure.test :refer :all]
             [clojure-learning.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
-
 (deftest abs-test
   (is (= 2 (abs -2))))
 
 (deftest square-test
   (is (= 4 (square 2))))
+
+(deftest good-enough-test-good
+  (is (= true (good_enough 4 2.0001))))
+
+(deftest good-enough-test-sad
+  (is (= false (good_enough 4 2.001))))
+
+(deftest improve-test
+  (is (= 5/2 (improve 4 1))))
+
+(defn can-accept
+  [x]
+  (< (abs x) 0.01))
+
+(deftest sqrt-test
+  (is (can-accept
+       (- 2 (sqrt 4 1)))
+  )
+)
