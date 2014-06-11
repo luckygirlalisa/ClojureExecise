4f3264c7f1e59316a1ec5f9c7f4f4f88db453a25
luckygirlalisa <zfxiao@thoughtworks.com>
2014-06-11 19:22:40 +0800

    use recursion caculate factorial

--- factorial/src/factorial/core.clj
+++ factorial/src/factorial/core.clj
@@ -1,6 +1,23 @@
 (ns factorial.core
   (:gen-class))
 
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn caculate-positive-factorial
+  [x]
+  (cond
+   (> x 1) (* x (caculate-positive-factorial (- x 1)))
+   :else 1
+   ))
+
 (defn factorial
   [x]
-  1)
+  (cond
+    (< x 0) (throw (Exception. "Negative does not have factorial."))
+   :else (caculate-positive-factorial x)
+  )
+)
+(factorial 4)
--- factorial/test/factorial/core_test.clj
+++ factorial/test/factorial/core_test.clj
@@ -2,5 +2,18 @@
   (:require [clojure.test :refer :all]
             [factorial.core :refer :all]))
 
+;(deftest factorial-test-negative
+ ; (is (thrown? Exception)))
+
+(deftest factorial-test-0
+  (is (= 1 (factorial 0))))
+
 (deftest factorial-test
   (is (= 1 (factorial 1))))
+
+(deftest factorial-test1
+  (is (= 2 (factorial 2))))
+
+(deftest factorial-test2
+  (is (= 6 (factorial 3))))
+
